bee4dd170c161d1b55ddc4f1a493978512d06c03
Je-el <jewelnmaldo@gmail.com>
2025-03-23 08:50:43 -0600

    react trial 2

---
+++ artcalc/src/pages/PrivacyPolicy.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function PrivacyPolicy() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Privacy Policy</h1>
+      <p>Please review our privacy policy below:</p>
+      <a 
+        href="https://docs.google.com/document/d/1ka0ehYIXJQLX5apXTEH_jwF1bO7TGIedRWyUEPFbDqs/edit?tab=t.0#heading=h.p9mhwcqlmsb9" 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        View Privacy Policy Document
+      </a>
+    </div>
+  );
+}
+
+export default PrivacyPolicy;
---
+++ artcalc/src/pages/Support.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Support() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Support</h1>
+      <p>
+        If you need support, please contact us at <a href="mailto:support@artcalc.com">support@artcalc.com</a>.
+      </p>
+      <img src="assets/placeholder.jpg" alt="Support placeholder" style={{ maxWidth: '300px' }} />
+    </div>
+  );
+}
+
+export default Support;
